49dbfd641875250ac2a815c0920390ab9d047ecd
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-06-10 21:31:48 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,54 @@
-import { Assets as NavigationAssets } from '@react-navigation/elements';
-import { Asset } from 'expo-asset';
-import * as SplashScreen from 'expo-splash-screen';
-import * as React from 'react';
-import { Navigation } from './navigation';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-Asset.loadAsync([
-  ...NavigationAssets,
-  require('./assets/newspaper.png'),
-  require('./assets/bell.png'),
-]);
+import * as SplashScreen from "expo-splash-screen";
+import * as React from "react";
+import { Navigation } from "./navigation";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 SplashScreen.preventAutoHideAsync();
 
 export function App() {
+  const [loaded, error] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+  
+  React.useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <Navigation
-      linking={{
-        enabled: 'auto',
-        prefixes: [
-          // Change the scheme to match your app's scheme defined in app.json
-          'helloworld://',
-        ],
-      }}
-      onReady={() => {
-        SplashScreen.hideAsync();
-      }}
-    />
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <StatusBar barStyle="light-content" />
+        <SafeAreaView style={{ flex: 1 }}>
+          <Navigation
+            linking={{
+              enabled: "auto",
+              prefixes: [
+                // Change the scheme to match your app's scheme defined in app.json
+                "helloworld://",
+              ],
+            }}
+            onReady={() => {
+              SplashScreen.hideAsync();
+            }}
+          />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ThemeProvider>
   );
 }
---
+++ src/navigation/screens/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import * as S from "./styles";
+
+export default function Groups() {
+  return (
+    <S.Container>
+      <Text>Test</Text>
+    </S.Container>
+  )
+}
